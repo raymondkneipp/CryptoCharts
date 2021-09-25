5b835d60d8b78491b07b17f8e07e40b42f1dbc59
Raymond Kneipp <raymond.f.kneipp@gmail.com>
2021-09-25 21:05:29 +0300

    removed test

--- src/App.js
+++ src/App.js
@@ -12,8 +12,6 @@ import {
 import "./App.css";
 
 const App = () => {
-  console.log(process.env.REACT_APP_TEST);
-
   return (
     <div className="grid grid-flow-row gap-10 pt-10">
       <Navbar />
